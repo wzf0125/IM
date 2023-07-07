2b4aa5db67a35ec00e187d09f0fd8b63549120ec
WZF <974500760@qq.com>
2023-07-07 22:32:43 +0800

    tmp: 暂存一下代码

--- IM-server/src/main/java/org/quanta/im/server/WebSocketHandler.java
+++ IM-server/src/main/java/org/quanta/im/server/WebSocketHandler.java
@@ -1,25 +1,24 @@
 package org.quanta.im.server;
 
 import cn.hutool.json.JSONUtil;
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpHeaderNames;
-import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.util.AttributeKey;
 import lombok.extern.log4j.Log4j2;
 import org.quanta.im.bean.MessageRequest;
 import org.quanta.im.bean.MessageResponse;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Log4j2
-public class WebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+public class WebSocketHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
     /**
      * 消息编号
      */
@@ -30,53 +29,48 @@ public class WebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketF
      */
     private static final Map<Channel, String> userMap = new HashMap<>();
 
-    /**
-     * 握手阶段
-     */
     @Override
-    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
+        // 判断是否是关闭连接的指令
+        if (frame instanceof CloseWebSocketFrame) {
+            // 关闭WebSocket连接
+            ctx.close();
+        } else if (frame instanceof TextWebSocketFrame) {
+            // 处理文本消息
+            TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
+            String message = textFrame.text();
+            FullHttpRequest request = (FullHttpRequest) ctx.channel().attr(AttributeKey.valueOf("request")).get();
+            // 获取请求参数
+            String username = getUsername("username", request);
 
+            if (username == null) {
+                broadcastMessage("非法请求");
+            }
+
+            // 注册用户
+            if (!userMap.containsKey(ctx.channel())) {
+                userMap.put(ctx.channel(), username);
+                log.info("用户登陆: [{}]", username);
+                broadcastMessage("欢迎用户 [" + username + "] 进入聊天室");
+            }
+
+            // 获取消息结构
+            MessageRequest content = JSONUtil.toBean(message, MessageRequest.class);
+
+            if (!userMap.get(ctx.channel()).equals(content.getUsername())) {
+                log.info("用户名更改 [{}] -> [{}]", username, content.getUsername());
+                broadcastMessage("用户名更新: [" + username + "] -> [" + content.getUsername() + "]");
+            }
+            broadcastMessage(username+" : "+content.getMessage());
+        }
     }
 
-    @Override
-    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame frame) throws Exception {
-//        // 切换到 WebSocket 协议
-//        WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
-//                getWebSocketLocation(frame), null, true);
-        String content = frame.text();
-        // Deserialize the message request
-        MessageRequest request = JSONUtil.toBean(content, MessageRequest.class);
-        String username = request.getUsername();
-        if (!userMap.containsKey(ctx.channel())) {
-            userMap.put(ctx.channel(), request.getUsername());
-            broadcastMessage("Welcome user:" + request.getUsername() + "to the chat room");
-        }
-
-        if (!userMap.get(ctx.channel()).equals(request.getUsername())) {
-            log.error("用户名改变:[{}] -> [{}]", username, request.getUsername());
-            userMap.put(ctx.channel(), request.getUsername());
-        }
-        userMap.put(ctx.channel(), request.getUsername());
-        String message = request.getMessage();
-
-        // Broadcast the message to all users
-        broadcastMessage(message);
-    }
-
-    @Override
-    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-        Channel channel = ctx.channel();
-        String username = userMap.remove(channel);
-        if (username != null) {
-            log.info("User unregistered: {}", username);
-            // Broadcast user exit message
-            broadcastMessage("User: [" + username + "] left the chat room");
-        }
-        super.channelUnregistered(ctx);
+    private String getUsername(String key, FullHttpRequest request) {
+        return request.headers().get(key);
     }
 
     private void broadcastMessage(String message) {
-        MessageResponse response = new MessageResponse(message);
+        MessageResponse response = new MessageResponse("["+atomicInteger.getAndIncrement()+"]--> "+message);
         String jsonResponse = JSONUtil.toJsonStr(response);
 
         for (Channel channel : userMap.keySet()) {
@@ -84,31 +78,10 @@ public class WebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketF
         }
     }
 
+
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        log.error("server catch exception", cause);
-        Channel channel = ctx.channel();
-        String username = userMap.remove(channel);
-        if (username != null) {
-            // Broadcast user exit message
-            broadcastMessage("User: [" + username + "] left the chat room");
-        }
+        cause.printStackTrace();
         ctx.close();
-    }
-
-    private String extractUsernameFromQueryParameters(String uri) {
-        // 提取查询参数中的用户名信息
-        QueryStringDecoder decoder = new QueryStringDecoder(uri);
-        Map<String, List<String>> parameters = decoder.parameters();
-        if (parameters.containsKey("username")) {
-            return parameters.get("username").get(0);
-        }
-        return "";
-    }
-
-    private String getWebSocketLocation(FullHttpRequest req) {
-        String protocol = "ws";
-        String host = req.headers().get(HttpHeaderNames.HOST);
-        return protocol + "://" + host + req.uri();
     }
 }
